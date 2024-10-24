811281a0b7faaf5f2a09004288c0b4c768394b49
zrygan <zr.gntn@gmail.com>
2024-10-24 18:33:13 +0800

    Refactor lengthOfLastWord function in 56.py

---
+++ 56 (Length of Last Word, String Handling)/56.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def lengthOfLastWord(self, s):
+        """
+            we can simply get an array of words from the string that
+            is separated by a space ' '
+            
+            IDEA (1)    convert string to array of words for each element
+                        in the array is a substring enclosed by ' '
+            
+            issue, this will also take "   " as a string in since it is a
+            space enclosed by two spaces
+            
+            IDEA (2)    only include the word in the array of words
+                        if it is not a ' ' or a space character
+                        
+            using array indexing from the right, we can get the length of the
+            right most element of the array of words
+            
+            IDEA (3)    using array indexing from the right, get the length of the
+                        right most element in the array of words
+        """
+        
+        return len([word for word in s.split(' ') if word != ''][-1])
