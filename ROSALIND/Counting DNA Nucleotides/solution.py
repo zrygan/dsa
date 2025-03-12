8c619e5d74b6ec11d74021d8d9c12c0cf5c4a6ad
Zhean Ganituen <zr.gntn@gmail.com>
2025-03-12 16:46:09 +0000

    Refactor nucleotide counting and add DNA to RNA transcription solution

--- ROSALIND/Counting DNA Nucleotides/solution.py
+++ ROSALIND/Counting DNA Nucleotides/solution.py
@@ -5,23 +5,29 @@
 
 import fileinput
 
-count_a = 0 
-count_c = 0 
-count_g = 0 
-count_t = 0 
-dna = ''
+class Solution:
+    @staticmethod
+    def nucleotide_count(dna: str) -> list[int, int, int, int]:
+        count_a = 0
+        count_c = 0 
+        count_g = 0 
+        count_t = 0 
+
+        for char in dna:
+            if char == "A":
+                count_a += 1
+            elif char == "C":
+                count_c += 1
+            elif char == "G":
+                count_g += 1
+            elif char == "T":
+                count_t += 1
+        
+        return count_a, count_c, count_g, count_t
+
+dna = None
 
 with open('dataset.txt', 'r') as f:
     dna = f.read()
 
-for char in dna:
-    if char == "A":
-        count_a += 1
-    elif char == "C":
-        count_c += 1
-    elif char == "G":
-        count_g += 1
-    elif char == "T":
-        count_t += 1
-
-print(count_a, count_c, count_g, count_t)
+print(Solution.nucleotide_count(dna))
